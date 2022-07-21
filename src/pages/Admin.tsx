f3f2a8f1e5cf3aafb0646225e1347f5af93b1e9d
zhoubin6 <zhoubin6@tal.com>
2022-07-21 18:54:09 +0800

    初始化

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,6 @@
+
+export default function Admin() {
+    return (
+        <div>admin界面</div>
+    )
+} 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+function Login<Component >() {
+    return (
+        <div>login页面</div>
+    )
+}
+
+export default Login;
